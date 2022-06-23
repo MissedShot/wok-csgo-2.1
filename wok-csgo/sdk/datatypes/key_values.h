f0f8b497559ae29c1c16bb93858d6149be0b6a09
MissedShot <missedshotik@gmail.com>
2022-06-24 02:51:25 +0300

    -updated under csgo update

--- wok-csgo/sdk/datatypes/key_values.h
+++ wok-csgo/sdk/datatypes/key_values.h
@@ -18,7 +18,7 @@ class c_key_values {
 public:
 	~c_key_values();
 
-	VFUNC_SIG(init(const char* key_name), "client.dll", "55 8B EC 51 33 C0 C7 45", void(__thiscall*)(void*, const char*), key_name)
+	VFUNC_SIG(init(const char* key_name), "client.dll", "55 8B EC 51 33 C0 C7 45", void(__thiscall*)(void*, const char*, int, int), key_name, 0, 0)
 	VFUNC_SIG(load_from_buffer(const char* resource_name, const char* buf, void* file_sys = nullptr, const char* path_id = nullptr, void* eval_sym_proc = nullptr, void* unk = nullptr), 
 		"client.dll", "55 8B EC 83 E4 F8 83 EC 34 53 8B 5D 0C 89", void(__thiscall*)(void*, const char*, const char*, void*, const char*, void*, void*), resource_name, buf, file_sys, path_id, eval_sym_proc, unk)
 	VFUNC_SIG(find_key(const char* name, bool create), "client.dll", "55 8B EC 83 EC 1C 53 8B D9 85 DB", c_key_values*(__thiscall*)(void*, const char*, bool), name, create)
